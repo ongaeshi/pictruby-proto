1c3bbcaad2277476b30aa9b3c9baf43e1ca6cd49
ongaeshi <ongaeshi0621@gmail.com>
2015-06-10 01:05:03 +0900

    Import GUI

--- pictruby/AppDelegate.m
+++ pictruby/AppDelegate.m
@@ -13,18 +13,18 @@
     tabBarController = [[UITabBarController alloc] init];
     tabBarController.title = @" ";
 
-    // UIViewController* view1 = [[FileViewController alloc]init];
-    // UIImage* icon1 = [UIImage imageNamed:@"tabbar_files.png"];
-    // view1.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"File" image:icon1 tag:0];
-    // UINavigationController *tab1 = [[UINavigationController alloc] initWithRootViewController:view1];
+    UIViewController* view1 = [[FileViewController alloc]init];
+    UIImage* icon1 = [UIImage imageNamed:@"tabbar_files.png"];
+    view1.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"File" image:icon1 tag:0];
+    UINavigationController *tab1 = [[UINavigationController alloc] initWithRootViewController:view1];
 
-    // UIViewController* view2 = [[SampleViewController alloc]init];
-    // UIImage* icon2 = [UIImage imageNamed:@"tabbar_samples.png"];
-    // view2.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"Sample" image:icon2 tag:0];
-    // UINavigationController *tab2 = [[UINavigationController alloc] initWithRootViewController:view2];
+    UIViewController* view2 = [[SampleViewController alloc]init];
+    UIImage* icon2 = [UIImage imageNamed:@"tabbar_samples.png"];
+    view2.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"Sample" image:icon2 tag:0];
+    UINavigationController *tab2 = [[UINavigationController alloc] initWithRootViewController:view2];
 
-    // NSArray* tabs = [NSArray arrayWithObjects:tab1, tab2, nil];
-    // [tabBarController setViewControllers:tabs animated:NO];
+    NSArray* tabs = [NSArray arrayWithObjects:tab1, tab2, nil];
+    [tabBarController setViewControllers:tabs animated:NO];
 
     [self.window addSubview:tabBarController.view];
     [self.window makeKeyAndVisible];
