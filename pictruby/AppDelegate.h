03d675b4d85ef35e672f1debf648083d9bb16280
ongaeshi <ongaeshi0621@gmail.com>
2015-06-10 00:35:59 +0900

    Initial Commit

---
+++ pictruby/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pictruby
+//
+//  Created by ongaeshi on 2015/06/10.
+//  Copyright (c) 2015年 ongaeshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pictruby/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pictruby
+//
+//  Created by ongaeshi on 2015/06/10.
+//  Copyright (c) 2015年 ongaeshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
